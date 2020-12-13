3cad58341710e009e0aebfc381ac06497833096c
matheus.souza2 <matheus.souza@acad.ifma.edu.br>
2020-12-13 01:59:33 -0300

    initial commit

---
+++ Cliente.js
@@ -0,0 +1,19 @@
+class Cliente{
+    _nome;
+    _cpf;
+
+    get cpf(){
+       return this._cpf;
+    }
+
+    get nome(){
+        return this._nome;
+    }
+
+    constructor(nome, cpf){
+        this._nome = nome;
+        this._cpf = cpf;
+    }
+}
+
+module.exports = { Cliente }
